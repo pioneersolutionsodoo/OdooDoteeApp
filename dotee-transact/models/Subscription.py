96306d99c39c6793dead2c44899cdb70a5c77703
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2020-06-05 17:39:02 +0100

    Update Subscription.py

--- dotee-transact/models/Subscription.py
+++ dotee-transact/models/Subscription.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import api, fields, models, api, _
 
 class Subscription(models.Model):
     _inherit = 'sale.subscription'
