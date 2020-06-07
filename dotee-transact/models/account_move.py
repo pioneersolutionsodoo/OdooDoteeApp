19255a426bcd4b50c51ac93d88f80bf627b81783
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2020-06-07 16:59:06 +0100

    Update account_move.py

--- dotee-transact/models/account_move.py
+++ dotee-transact/models/account_move.py
@@ -38,15 +38,15 @@ class AccountMoveLine(models.Model):
             # })  
     
     
-    @api.depends('unit_vacant', 'price_unit', 'quantity', 'discount', 'analytic_account_id.pricelist_id')
-    def _compute_price_subtotal(self):
-        AccountTax = self.env['account.tax']
-        for line in self:
-            price = AccountTax._fix_tax_included_price(line.price_unit, line.product_id.sudo().taxes_id, AccountTax)
-            line.price_subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
-            line.price_subtotal -= float(line.unit_vacant * line.price_unit)
-            if line.analytic_account_id.pricelist_id.sudo().currency_id:
-                line.price_subtotal = line.analytic_account_id.pricelist_id.sudo().currency_id.round(line.price_subtotal)
+    # @api.depends('unit_vacant', 'price_unit', 'quantity', 'discount', 'analytic_account_id.pricelist_id')
+    # def _compute_price_subtotal(self):
+    #     AccountTax = self.env['account.tax']
+    #     for line in self:
+    #         price = AccountTax._fix_tax_included_price(line.price_unit, line.product_id.sudo().taxes_id, AccountTax)
+    #         line.price_subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
+    #         line.price_subtotal -= float(line.unit_vacant * line.price_unit)
+    #         if line.analytic_account_id.pricelist_id.sudo().currency_id:
+    #             line.price_subtotal = line.analytic_account_id.pricelist_id.sudo().currency_id.round(line.price_subtotal)
 
 
 
