fc040becc7eaaa05ccd3c78c63e18f6301bbe035
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2020-06-09 22:26:50 +0100

    all changes

--- dotee-transact/models/Subscription.py
+++ dotee-transact/models/Subscription.py
@@ -1,31 +1,64 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, api, _
+from dateutil.relativedelta import relativedelta
 from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools import format_date, float_compare
 # from odoo import models, fields, api
 
 class Subscription(models.Model):
     _inherit = 'sale.subscription'
 
     period = fields.Integer('Number of months', default=1)
+    
+    def _prepare_dotee_invoice_data(self):
+        res = self._prepare_invoice_data()
+        res['period'] = self.period
+        return res
+        
+    def _prepare_dotee_invoice_line(self, line, fiscal_position, date_start=False, date_stop=False):
+        res = self._prepare_invoice_line(line, fiscal_position, date_start, date_stop)
+#         if(line.period > 1):
+#             res['price_unit'] = line.price_unit * line.period
+        quantity = line.quantity - line.unit_vacant
+        if(line.period > 1):
+            quantity = quantity * line.period
+        res['quantity'] = quantity
+        res['unit_vacant'] = line.unit_vacant
+        res['period'] = line.period
+        return res
+    
+    def _prepare_dotee_invoice_lines(self, fiscal_position):
+        self.ensure_one()
+        revenue_date_start = self.recurring_next_date
+        periods = {'daily': 'days', 'weekly': 'weeks', 'monthly': 'months', 'yearly': 'years'}
+        revenue_date_stop = revenue_date_start + relativedelta(**{periods[self.recurring_rule_type]: self.recurring_interval}) - relativedelta(days=1)
+        return [(0, 0, self._prepare_dotee_invoice_line(line, fiscal_position, revenue_date_start, revenue_date_stop)) for line in self.recurring_invoice_line_ids]
+
+    
+    def _prepare_invoice(self):
+        invoice = self._prepare_dotee_invoice_data()
+        invoice['invoice_line_ids'] = self._prepare_dotee_invoice_lines(invoice['fiscal_position_id'])
+        return invoice
 
 class SubscriptionLine(models.Model):
 #     _name = 'dotee-transact.dotee-transact'
 #     _description = 'dotee-transact.dotee-transact'
     _inherit = 'sale.subscription.line'
     
-    unit_vacant = fields.Integer('Vacant', store=True)
+    unit_vacant = fields.Float('Vacant Unit', required=True, digits='Product Price')
     period = fields.Integer(related='analytic_account_id.period', store=True)
     price_subtotal = fields.Float(compute='_compute_price_subtotal', string='Subtotal', digits='Account', store=True)
 
     @api.onchange('period')
-    @api.depends('unit_vacant', 'price_unit', 'period')
-    def _compute_price_unit(self):
+    @api.depends('price_unit', 'period')
+    def _compute_period_price(self):
         for line in self:
-            line.price_unit = line.price_unit * line.period
-            line.update({
-                'price_unit' : line.price_unit
-            })
+            if(line.period > 1):
+                line.price_unit = line.price_unit * line.period
+                line.write({
+                    'price_unit' : line.price_unit
+                })
 
     @api.onchange ('unit_vacant')
     @api.depends('price_unit', 'unit_vacant', 'price_subtotal', 'quantity', 'discount', 'analytic_account_id.pricelist_id')
@@ -36,8 +69,11 @@ class SubscriptionLine(models.Model):
             if(line.quantity < line.unit_vacant):
                 raise ValidationError("Vacant cannot be greater than quantity.")
             price = AccountTax._fix_tax_included_price(line.price_unit, line.product_id.sudo().taxes_id, AccountTax)
-            line.price_subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
-            line.price_subtotal -= float(line.unit_vacant * line.price_unit)
+            subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
+            subtotal = subtotal - float(line.unit_vacant * line.price_unit)
+            if(line.period > 1):
+                subtotal = subtotal * line.period
+            line.price_subtotal = subtotal
                 
             # price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
             # if line.product_uom_qty and line.receive_in_advance:
@@ -50,12 +86,16 @@ class SubscriptionLine(models.Model):
                 'price_subtotal': line.price_subtotal
             })  
 
-    @api.depends('unit_vacant', 'price_unit', 'quantity', 'discount', 'analytic_account_id.pricelist_id')
+    @api.depends('unit_vacant', 'price_unit', 'quantity', 'discount', 'analytic_account_id.pricelist_id', 'period')
     def _compute_price_subtotal(self):
         AccountTax = self.env['account.tax']
         for line in self:
             price = AccountTax._fix_tax_included_price(line.price_unit, line.product_id.sudo().taxes_id, AccountTax)
-            line.price_subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
-            line.price_subtotal -= float(line.unit_vacant * line.price_unit)
+            subtotal = line.quantity * price * (100.0 - line.discount) / 100.0
+            subtotal = subtotal - float(line.unit_vacant * line.price_unit)
+            if(line.period > 1):
+                subtotal = subtotal * line.period
+            line.price_subtotal = subtotal
+            
             if line.analytic_account_id.pricelist_id.sudo().currency_id:
                 line.price_subtotal = line.analytic_account_id.pricelist_id.sudo().currency_id.round(line.price_subtotal)
