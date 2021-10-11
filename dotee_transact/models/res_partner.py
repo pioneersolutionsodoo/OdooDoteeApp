34c47fa1f14ea85f87ba1ee4567aa1adac54deb8
Chigozirim Ogbogu <cogbogu@plexada-si.com>
2021-10-11 09:58:15 +0100

    updated res_partner

--- dotee_transact/models/res_partner.py
+++ dotee_transact/models/res_partner.py
@@ -8,8 +8,14 @@ class Partner(models.Model):
 #     _description = 'dotee-transact.dotee-transact'
     _inherit = "res.partner"
 
-    x_customer_payment_id = fields.Char('Property Code')
+    #x_customer_payment_id = fields.Char('Property Code')
+    full_formatted_address = fields.Char("Full Address", compute="_get_full_address")
     
+    @api.depends('street', 'x_studio_wards_2', 'x_studio_provinces_2')
+    def _get_full_address(self):
+        for record in self:
+            record['full_formatted_address'] = str(record.street) + '\r\n' + str(record.x_studio_wards_2.x_name) + '\r\n' + str(record.x_studio_provinces_2.x_name)
+    # x_studio_computed_charge = fields.Monetary()
 
     # x_studio_computed_charge = fields.Monetary()
 
